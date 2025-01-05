b85b5cdb405b600897afbc1157b929ed30bca5e9
ritvikra <ritvikrallapalli@gmail.com>
2025-01-04 18:27:15 -0600

    scraper date deleted

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -1,7 +1,6 @@
 import psycopg2
 import requests
 from bs4 import BeautifulSoup
-from datetime import datetime
 
 
 # PostgreSQL connection setup
@@ -21,25 +20,19 @@ def scrapenfldata():
     soup = BeautifulSoup(response.text, "html.parser")
     total_table = soup.find("table", id="spread-total-table")
     odds_table = total_table.find("tbody", class_="odds-tbody")
-    tables = odds_table.find_all("tr", class_ ="oddsGameRow")
-    games_data = [] 
+    tables = odds_table.find_all("tr", class_="oddsGameRow")
+    games_data = []
 
     for table in tables:
-        dateofgame = table.find("div", class_="td-cell game-time")
-        date_part = dateofgame.find_all("span")[0].text.strip()  
-        time_part = dateofgame.find_all("span")[1].text.strip() 
-        curr_year = 2025
-        datetime_string = f"{curr_year} {date_part} {time_part}" 
-        sql_friendly_datetime = datetime.strptime(datetime_string, "%Y %b %d, %I:%M %p").strftime("%Y-%m-%d %H:%M:%S")
-        print(f"Date and Time: {sql_friendly_datetime}")
         away_team = table.find("div", class_='td-cell away-cell')
         if away_team:
-            away_team = away_team.find("strong" ).text
-            print(f"Away Team: {away_team} ")
+            away_team = away_team.find("strong").text
+            print(f"Away Team: {away_team}")
         home_team = table.find("div", class_='td-cell home-cell')
         if home_team:
-            home_team = home_team.find("strong" ).text
-            print(f"Home Team: {home_team} ")  
+            home_team = home_team.find("strong").text
+            print(f"Home Team: {home_team}")
+
         books = table.find_all("td", class_="liveOddsCell")
         game_odds = []
         count = 0
@@ -60,23 +53,22 @@ def scrapenfldata():
             print(f"Spread: {spreadline}", f"Odds: {spreadodds}")
             count += 1
             game_odds.append({
-                    "sportsbook": book_name,
-                    "type": "total",
-                    "value": float(overline),  # Extract numeric part from 'o 41.5'
-                    "odds": int(overodds)
+                "sportsbook": book_name,
+                "type": "total",
+                "value": float(overline),  # Extract numeric part from 'o 41.5'
+                "odds": int(overodds)
             })
             game_odds.append({
-                    "sportsbook": book_name,
-                    "type": "spread",
-                    "value": float(spreadline),  # Assuming it's already a numeric string
-                    "odds": int(spreadodds)
+                "sportsbook": book_name,
+                "type": "spread",
+                "value": float(spreadline),  # Assuming it's already a numeric string
+                "odds": int(spreadodds)
             })
         games_data.append({
-        "league": "NFL",
-        "date": sql_friendly_datetime,
-        "home_team": home_team,
-        "away_team": away_team,
-        "odds": game_odds
+            "league": "NFL",
+            "home_team": home_team,
+            "away_team": away_team,
+            "odds": game_odds
         })
     return games_data
 
@@ -93,11 +85,11 @@ def insert_data(data):
         # Insert game
         cursor.execute(
             """
-            INSERT INTO games (league_id, date, home_team, away_team) 
-            VALUES (%s, %s, %s, %s) 
+            INSERT INTO games (league_id, home_team, away_team) 
+            VALUES (%s, %s, %s) 
             RETURNING id
             """,
-            (league_id, game["date"], game["home_team"], game["away_team"])
+            (league_id, game["home_team"], game["away_team"])
         )
         game_id = cursor.fetchone()[0]
 
@@ -125,4 +117,4 @@ if __name__ == "__main__":
         conn.rollback()  # Roll back the transaction in case of an error
     finally:
         cursor.close()
-        conn.close()  # Always close the connection`
+        conn.close()  # Always close the connection
